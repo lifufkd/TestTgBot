a7252e66efb173a64bc95518ba2b1eb0e5fce11f
lilfuz <lilfuz@gmail.com>
2024-03-01 18:42:02 +0300

    I won.

--- backend.py
+++ backend.py
@@ -7,6 +7,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 from datetime import datetime
+from PIL import Image
 #####################################
 
 
@@ -17,7 +18,7 @@ class TempUserData:
 
     def temp_data(self, user_id):
         if user_id not in self.__user_data.keys():
-            self.__user_data.update({user_id: [None, [None, None, None, None]]}) # 1 - status, 2 - m
+            self.__user_data.update({user_id: [None, [None, None, None, None, None], None]}) # 1 - status, 2 - m
         return self.__user_data
 
 
@@ -27,17 +28,26 @@ class ExcellImport:
         self.__db = db
         self.__sheet = None
         self.init()
-        self.excell()
 
     def init(self):
         creds = ServiceAccountCredentials.from_json_keyfile_name('creditionals.json')
         gc = gspread.authorize(creds)
         self.__sheet = gc.open("Бот для продаж")
 
-    def excell(self):
+    def products_excell(self):
         worksheet = self.__sheet.get_worksheet(1) # 1 - вторая страница для парсинга
         data = worksheet.get_all_values()
-        print(data)
+        return data[1:]
+
+    def categories_excell(self):
+        worksheet = self.__sheet.get_worksheet(2) # 1 - вторая страница для парсинга
+        data = worksheet.get_all_values()
+        return data[1:]
+
+    def subcategories_excell(self):
+        worksheet = self.__sheet.get_worksheet(3) # 1 - вторая страница для парсинга
+        data = worksheet.get_all_values()
+        return data[1:]
 
 
 class DbAct:
@@ -73,8 +83,57 @@ class DbAct:
             return status
 
     def add_one_product(self, data):
-        self.__db.db_write('INSERT INTO products (photo, price, key, description) VALUES (?, ?, ?, ?)', data)
+        self.__db.db_write(f'INSERT INTO products (photo, price, key, description, category, purchased) VALUES (?, ?, ?, ?, ?, {False})', data)
 
-    def update_db_from_excell(self):
-        pass
+    def update_products_from_excell(self, data):
+        check = self.__db.db_read(f'SELECT price, key, category, description FROM products', ())
+        for i in data:
+            try:
+                i[1] = int(i[1])
+                if tuple(i[1:]) not in check:
+                    print(tuple(i[1:]), 'in')
+                    self.__db.db_write(f'INSERT INTO products (photo, price, key, category, description, purchased) VALUES (?, ?, ?, ?, ?, ?)', (open('no-photo.png', 'rb'), int(i[1]), i[2], i[3], i[4], False))
+            except:
+                pass
+
+    def update_categories_from_excell(self, data):
+        check = self.__db.db_read(f'SELECT id, name FROM categories', ())
+        for i in data:
+            if tuple(i) not in check:
+                self.__db.db_write(f'INSERT INTO categories (id, name) VALUES (?, ?)', i)
+
+    def update_subcategories_from_excell(self, data):
+        check = self.__db.db_read(f'SELECT id, id_categories, name FROM subcategories', ())
+        for i in data:
+            if tuple(i) not in check:
+                self.__db.db_write(f'INSERT INTO subcategories (id, id_categories, name) VALUES (?, ?, ?)', i)
+
+    def get_categories(self):
+        data = self.__db.db_read('SELECT id, name FROM categories', ())
+        return data
+
+    def get_sub_by_id_categories(self, id_categories):
+        data = self.__db.db_read('SELECT id, name FROM subcategories WHERE id_categories = ?', (id_categories, ))
+        return data
+
+    def get_subcategories(self):
+        data = self.__db.db_read('SELECT id, name FROM subcategories', ())
+        return data
+
+    def get_products_by_id(self, id_product):
+        data = self.__db.db_read('SELECT row_id, photo, price, key, description FROM products WHERE category = ? AND purchased = 0', (id_product, ))
+        return data
+
+    def update_product(self, data, field, product_id):
+        self.__db.db_write(f'UPDATE products SET {field} = ? WHERE row_id = ?', (data, product_id))
+
+    def check_product_id_exist(self, product_id):
+        data = self.__db.db_read('SELECT count(*) FROM products WHERE row_id = ?', (product_id,))
+        if len(data) > 0:
+            if data[0][0] > 0:
+                status = True
+            else:
+                status = False
+            return status
+
 
--- main.py
+++ main.py
@@ -14,10 +14,15 @@ from frontend import Bot_inline_btns
 
 ####################################################################
 config_name = 'secrets.json'
-
-
 ####################################################################
 
+
+def get_subcot():
+    s = ''
+    data = db_actions.get_subcategories()
+    for i in data:
+        s += f'{i[0]} - {i[1]}\n'
+    return s
 
 def main():
     @bot.message_handler(commands=['start'])
@@ -85,11 +90,75 @@ def main():
                 elif status == 3:
                     if user_input is not None:
                         temp_user_data.temp_data(user_id)[user_id][1][3] = user_input
+                        temp_user_data.temp_data(user_id)[user_id][0] = 4
+                        bot.send_message(message.chat.id, f'Отправьте ID подкатегории\nДоступные варианты:\n{get_subcot()}')
+                    else:
+                        bot.send_message(message.chat.id, 'Это не текст')
+                elif status == 4:
+                    if user_input is not None:
+                        temp_user_data.temp_data(user_id)[user_id][1][4] = user_input
                         temp_user_data.temp_data(user_id)[user_id][0] = None
                         db_actions.add_one_product(temp_user_data.temp_data(user_id)[user_id][1])
                         bot.send_message(message.chat.id, 'Товар успешно добавлен!')
                     else:
                         bot.send_message(message.chat.id, 'Это не текст')
+                elif status == 5:
+                    if user_input is not None:
+                        if db_actions.check_product_id_exist(user_input):
+                            temp_user_data.temp_data(user_id)[user_id][2] = user_input
+                            temp_user_data.temp_data(user_id)[user_id][0] = None
+                            bot.send_message(message.chat.id, 'Что вы хотите изменить?',
+                                             reply_markup=buttons.change_btns())
+                        else:
+                            bot.send_message(message.chat.id, 'ID продукта не существует!')
+                    else:
+                        bot.send_message(message.chat.id, 'Это не текст')
+                elif status == 6:
+                    if user_input is not None:
+                        try:
+                            db_actions.update_product(int(user_input), 'price',
+                                                      temp_user_data.temp_data(user_id)[user_id][2])
+                            temp_user_data.temp_data(user_id)[user_id][0] = None
+                            bot.send_message(user_id, 'Операция завершена успешно!')
+                        except:
+                            bot.send_message(user_id, 'Сумма неверна!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст')
+                elif status == 7:
+                    if photo is not None:
+                        photo_id = photo[-1].file_id
+                        photo_file = bot.get_file(photo_id)
+                        photo_bytes = bot.download_file(photo_file.file_path)
+                        db_actions.update_product(photo_bytes, 'photo',
+                                                  temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Операция завершена успешно!')
+                    else:
+                        bot.send_message(user_id, 'Это не фото')
+                elif status == 8:
+                    if user_input is not None:
+                        db_actions.update_product(user_input, 'key',
+                                                  temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Операция завершена успешно!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст')
+                elif status == 9:
+                    if user_input is not None:
+                        db_actions.update_product(user_input, 'category',
+                                                  temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Операция завершена успешно!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст')
+                elif status == 10:
+                    if user_input is not None:
+                        db_actions.update_product(user_input, 'description',
+                                                  temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Операция завершена успешно!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст')
             else:
                 if message.text == 'Профиль':
                     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!',
@@ -97,7 +166,8 @@ def main():
                 elif message.text == 'Мои покупки':
                     bot.send_message(message.chat.id, 'Ваши покупки:\n1. Back4Blood')
                 elif message.text == 'Каталог продуктов':
-                    bot.send_message(message.chat.id, 'Выберите действие', reply_markup=buttons.categories_btns())
+                    categories = db_actions.get_categories()
+                    bot.send_message(message.chat.id, 'Выберите категорию', reply_markup=buttons.categories_btns(categories))
                 elif message.text == 'Поддержка':
                     bot.send_message(message.chat.id, 'Выберите действие', reply_markup=buttons.support_btns())
                 elif message.text == 'Наши контакты':
@@ -110,14 +180,55 @@ def main():
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
         command = call.data
+        user_input = call.message.text
+        photo = call.message.photo
+        message_id = call.message.id
         user_id = call.message.chat.id
         if db_actions.user_is_existed(user_id):
+            buttons = Bot_inline_btns()
             if db_actions.user_is_admin(user_id):
                 if command == 'addproduct':
                     temp_user_data.temp_data(user_id)[user_id][0] = 0
                     bot.send_message(call.message.chat.id, 'Отправьте фото товара')
-                elif command == 'importexcell':
-                    pass
+                elif command == 'importproducts':
+                    db_actions.update_products_from_excell(sheet.products_excell())
+                elif command == 'importcategories':
+                    db_actions.update_categories_from_excell(sheet.categories_excell())
+                elif command == 'importsubcategories':
+                    db_actions.update_subcategories_from_excell(sheet.subcategories_excell())
+                elif command == 'changeproduct':
+                    temp_user_data.temp_data(user_id)[user_id][0] = 5
+                    bot.send_message(call.message.chat.id, 'Введите ID товара')
+                elif command[:10] == 'сhangecart':
+                    if command[10:] == '1':
+                        temp_user_data.temp_data(user_id)[user_id][0] = 6
+                        bot.send_message(call.message.chat.id, 'Введите новую цену')
+                    elif command[10:] == '2':
+                        temp_user_data.temp_data(user_id)[user_id][0] = 7
+                        bot.send_message(call.message.chat.id, 'Введите новую обложку')
+                    elif command[10:] == '3':
+                        temp_user_data.temp_data(user_id)[user_id][0] = 8
+                        bot.send_message(call.message.chat.id, 'Введите новую ключ')
+                    elif command[10:] == '4':
+                        temp_user_data.temp_data(user_id)[user_id][0] = 9
+                        bot.send_message(call.message.chat.id, f'Введите новую подкатегорию для товара\nДоступные варианты:\n{get_subcot()}')
+                    elif command[10:] == '5':
+                        temp_user_data.temp_data(user_id)[user_id][0] = 10
+                        bot.send_message(call.message.chat.id, 'Введите новое описание')
+            if command[:10] == 'categories':
+                subcategories = db_actions.get_sub_by_id_categories(command[10:])
+                bot.edit_message_text('Выберите подкатегорию', user_id, message_id, reply_markup=buttons.subcategories_btns(subcategories))
+            elif command[:13] == 'subcategories':
+                if command[13:] == '<back>':
+                    categories = db_actions.get_categories()
+                    bot.edit_message_text('Выберите категорию', user_id, message_id,
+                                     reply_markup=buttons.categories_btns(categories))
+                else:
+                    products = db_actions.get_products_by_id(command[13:])
+                    for i in products:
+                        bot.send_photo(user_id, photo=i[1], caption=f'ID товара: {i[0]}\n{i[3]}\n{i[4]}\n{i[2]}', reply_markup=buttons.buy_btns(i[0]))
+        else:
+            bot.send_message(user_id, 'Введите /start для запуска бота')
 
     bot.polling(none_stop=True)
 
