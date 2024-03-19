391c51dc98d6b2703ef9dd8f6fce48ad10adc7ed
lilfuz <lilfuz@gmail.com>
2024-03-19 22:41:46 +0300

    I won.

--- data.py
+++ data.py
@@ -4,7 +4,30 @@ class Variants:
     def __init__(self):
         super(Variants, self).__init__()
 
-    def text_field_ids_1(self):
+    def cards_input_query(self, type, index):
         data = [
-
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{index}]/div/div",
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[{index}]/div/div/div/div",
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{index}]/div/div/div"
         ]
+        return data[type]
+
+    def cards_buttons_query(self, type, card, index):
+        data = [
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{card}]/div/div/div/div/div[2]/div[2]/div[{index}]/div[1]",
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div/div/div/div/div/div[2]/div[2]/div[{index}]/div[1]",
+        ]
+        return data[type]
+
+    def circles(self, type, index, card=None):
+        data = [
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[{index}]",
+            f"/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div[{card}]/div/div/div/div/div[2]/div[2]/div[1]/div/div/div[{index}]"
+        ]
+        return data[type]
+
+    def checker(self, type):
+        data = [
+            "/html/body/div[1]/div/div/div[1]/div[1]/span/div/div[1]/div/div/div/div"
+        ]
+        return data[type]
